a43896a8e2ab74e457a8ec4d3a9b9becc5bd47e4
joao <joao.freixial.silveira@tecnico.ulisboa.pt>
2022-02-05 10:06:31 +0000

    removed boost dependencies

--- src/cli.cpp
+++ src/cli.cpp
@@ -10,11 +10,22 @@
 #include <algorithm>
 #include <cctype>
 #include <locale>
-#include <boost/algorithm/string/classification.hpp>
-#include <boost/algorithm/string/split.hpp>
 #include <iostream>
 #include <string>
 #include <vector>
+
+// clang-format off
+const char* SQUARE_NAMES[] = {
+    "A1","B1","C1","D1","E1","F1","G1","H1",
+    "A2","B2","C2","D2","E2","F2","G2","H2",
+    "A3","B3","C3","D3","E3","F3","G3","H3",
+    "A4","B4","C4","D4","E4","F4","G4","H4",
+    "A5","B5","C5","D5","E5","F5","G5","H5",
+    "A6","B6","C6","D6","E6","F6","G6","H6",
+    "A7","B7","C7","D7","E7","F7","G7","H7",
+    "A8","B8","C8","D8","E8","F8","G8","H8",
+};
+// clang-format on
 
 bool doCommand(const std::string buf, Board &board);
 void infoCommand(const Board &board);
@@ -39,81 +50,60 @@ void cli::readCommands()
 
 bool doCommand(const std::string buf, Board &board)
 {
-    std::vector<std::string> tokens;
-    boost::split(tokens, buf, boost::is_any_of(" "));
-
-    if (tokens.size() == 1)
+    if (buf == "help" || buf == "h" || buf == "?")
     {
-        if (tokens[0] == "help" || tokens[0] == "h" || tokens[0] == "?")
-        {
-            std::cout << "\n"
-                      << "black               : BLACK to move\n"
-                      << "cc                  : play computer-to-computer \n"
-                      << "d                   : display board \n"
-                      << "eval                : show static evaluation of this position\n"
-                      << "exit                : exit program \n"
-                      << "game                : show game moves \n"
-                      << "go                  : computer next move \n"
-                      << "help                : Displays information about all the commands\n"
-                      << "help                : show this help \n"
-                      << "info                : display variables (for testing purposes)\n"
-                      << "move e2e4, or h7h8q : enter a move (use this format)\n"
-                      << "moves               : show all legal moves\n"
-                      << "new                 : start new game\n"
-                      << "perf                : benchmark a number of key functions\n"
-                      << "perft n             : calculate raw number of nodes from here, depth n \n"
-                      << "r                   : rotate board \n"
-                      << "readfen fen         : reads FEN position\n"
-                      << "sd n                : set the search depth to n\n"
-                      << "setup               : setup board... \n"
-                      << "undo                : take back last move\n"
-                      << "white               : WHITE to move"
-                      << std::endl;
-        }
-        else if (tokens[0] == "black")
-        {
-            board._white_to_move = false;
-        }
-        else if (tokens[0] == "d")
-        {
-            std::cout << board;
-        }
-        else if (tokens[0] == "info")
-        {
-            infoCommand(board);
-        }
-        else if (tokens[0] == "new")
-        {
-            board.init();
-        }
-        else if (tokens[0] == "r")
-        {
-            board._view_rotated = !board._view_rotated;
-            std::cout << board;
-        }
-        else if (tokens[0] == "white")
-        {
-            board._white_to_move = true;
-        }
-        else if ((tokens[0] == "exit"))
-        {
-            return false;
-        }
-        else
-        {
-            std::cout << "command unknown or not implemented: " << buf << ", type 'help' for more the available commands" << std::endl;
-        }
+        std::cout << "\n"
+                  << "black               : BLACK to move\n"
+                  << "cc                  : play computer-to-computer \n"
+                  << "d                   : display board \n"
+                  << "eval                : show static evaluation of this position\n"
+                  << "exit                : exit program \n"
+                  << "game                : show game moves \n"
+                  << "go                  : computer next move \n"
+                  << "help                : Displays information about all the commands\n"
+                  << "help                : show this help \n"
+                  << "info                : display variables (for testing purposes)\n"
+                  << "move e2e4, or h7h8q : enter a move (use this format)\n"
+                  << "moves               : show all legal moves\n"
+                  << "new                 : start new game\n"
+                  << "perf                : benchmark a number of key functions\n"
+                  << "perft n             : calculate raw number of nodes from here, depth n \n"
+                  << "r                   : rotate board \n"
+                  << "readfen fen         : reads FEN position\n"
+                  << "sd n                : set the search depth to n\n"
+                  << "setup               : setup board... \n"
+                  << "undo                : take back last move\n"
+                  << "white               : WHITE to move"
+                  << std::endl;
     }
-    else if (tokens.size() == 7)
+    else if (buf == "black")
     {
-        if (tokens[0] == "readfen")
-        {
-            board.initFromFen(tokens[1].c_str(), tokens[2].c_str(), tokens[3].c_str(), tokens[4].c_str(), tokens[5].c_str(), tokens[6].c_str());
-        }
-        else
-        {
-            std::cout << "command unknown or not implemented: " << buf << ", type 'help' for more the available commands" << std::endl;
-        }
+        board._white_to_move = false;
+    }
+    else if (buf == "d")
+    {
+        std::cout << board;
+    }
+    else if (buf == "info")
+    {
+        infoCommand(board);
+    }
+    else if (buf == "new")
+    {
+        board.init();
+    }
+    else if (buf == "r")
+    {
+        board._view_rotated = !board._view_rotated;
+        std::cout << board;
+    }
+    else if (buf == "white")
+    {
+        board._white_to_move = true;
+    }
+    else if ((buf == "exit"))
+    {
+        return false;
     }
     else
     {
@@ -124,18 +114,17 @@ bool doCommand(const std::string buf, Board &board)
 
 void infoCommand(const Board &board)
 {
-    std::cout << "\n============ info start ==============\n"
-              << "Size of board, in bytes   = " << sizeof(board)
-              << "\nMaterial value            = " << board._material
-              << "\nWhite castling rights     = " << board._castle_white
-              << "\nBlack castling rights     = " << board._castle_black
-              << "\nEn-passant square         = " << board._ep_square
-              << "\nFifty move count          = " << board._fifty_move
-              << "\nbitCnt of white pawns     = " << utils::bitCnt(board._white_pawns)
-              << "\nbitCnt of black pawns     = " << utils::bitCnt(board._black_pawns)
-              << "\nbitmap of board._occupied_squares:" << std::endl;
+    std::cout
+        << "Size of board, in bytes   = " << sizeof(board)
+        << "\nMaterial value            = " << board._material
+        << "\nWhite castling rights     = " << board._castle_white
+        << "\nBlack castling rights     = " << board._castle_black
+        << "\nEn-passant square         = " << board._ep_square
+        << "\nFifty move count          = " << board._fifty_move
+        << "\nbitCnt of white pawns     = " << utils::bitCntKernighan(board._white_pawns)
+        << "\nbitCnt of black pawns     = " << utils::bitCntKernighan(board._black_pawns)
+        << "\nbitmap of board._occupied_squares:" << std::endl;
     utils::printBB(board._occupied_squares);
-    std::cout << "============ info end ================\n";
 }
 
 void loadFenCommand(const Board &board)
