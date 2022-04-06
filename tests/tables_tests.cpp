31e2eabfd4624fb5e73077a378372612a4ef5b91
henrique <hdh.caraca@gmail.com>
2022-04-06 22:38:22 +0100

    Pawn attacks tests #4

--- tests/tables_tests.cpp
+++ tests/tables_tests.cpp
@@ -1,11 +1,13 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
+#include "../src/defs.hpp"
 #include "../src/magics.hpp"
 #include "../src/tables.hpp"
+#include "../src/board.hpp"
 
 void setup()
 {
+
     static bool init = false;
 
     if (!init)
@@ -17,6 +19,38 @@ void setup()
 }
 
 // Test Pawn
+
+TEST_CASE("Test Pawn Moves")
+{
+    setup();
+
+    SECTION("Corners Start")
+    {
+        REQUIRE(Tables::ATTACKS_PAWN[WHITE][A2] == 0x20000);
+        REQUIRE(Tables::ATTACKS_PAWN[WHITE][H2] == 0x400000);
+        REQUIRE(Tables::ATTACKS_PAWN[BLACK][A7] == 0x20000000000);
+        REQUIRE(Tables::ATTACKS_PAWN[BLACK][H7] == 0x400000000000);
+    }
+
+    SECTION("Limits White")
+    {
+        REQUIRE(Tables::ATTACKS_PAWN[WHITE][A8] == 0x0);
+        REQUIRE(Tables::ATTACKS_PAWN[WHITE][H8] == 0x0);
+    }
+
+    SECTION("Limits Black")
+    {
+        REQUIRE(Tables::ATTACKS_PAWN[BLACK][A1] == 0x0);
+        REQUIRE(Tables::ATTACKS_PAWN[BLACK][H1] == 0x0);
+    }
+
+    SECTION("Middle of the Board")
+    {
+        REQUIRE(Tables::ATTACKS_PAWN[WHITE][D4] == 0x1400000000);
+        REQUIRE(Tables::ATTACKS_PAWN[BLACK][E5] == 0x28000000);
+    }
+}
+
 // Test King
 
 TEST_CASE("Test King Moves")
@@ -36,6 +70,8 @@ TEST_CASE("Test King Moves")
         REQUIRE(Tables::ATTACKS_KING[D4] == 0x1c141c0000);
     }
 }
+
+// Test Knights
 
 TEST_CASE("Test Knights Moves")
 {
