a82ea2ed85855e161512f5774712a99da4578e6f
João Silveira <joao.freixial.silveira@tecnico.ulisboa.pt>
2022-04-14 23:46:20 +0100

    uci is now the default mode for the engine

--- src/main.cpp
+++ src/main.cpp
@@ -22,12 +22,15 @@ int main(int argc, char const *argv[])
   switch (parseOptions(argc, argv).interface)
   {
   case CLI:
+    std::cout << "Chess Engine Initialized in CLI Mode" << std::endl;
     Cli::init();
     break;
   case UCI:
+    std::cout << "Chess Engine Initialized in UCI Mode" << std::endl;
     Uci::init();
     break;
   default:
+    std::cout << "Chess Engine Initialized in UCI Mode" << std::endl;
     Uci::init();
     break;
   }
