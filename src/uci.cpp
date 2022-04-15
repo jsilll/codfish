5ea75ad81de9bae351767dd1f13f533e671fe7dc
João Silveira <joao.freixial.silveira@tecnico.ulisboa.pt>
2022-04-15 19:53:53 +0100

    minimal uci protocol #13

--- src/uci.cpp
+++ src/uci.cpp
@@ -14,233 +14,289 @@
 #include <string>
 #include <vector>
 
-// Modifies the board state
-void parseCommand(std::string &cmd, Board &board);
-void positionCommand(std::vector<std::string> &args, Board &board);
-void movesSubCommand(std::vector<std::string> moves, Board &board);
-void parseFen(std::string &piece_placements, std::string &active_color,
-              std::string &castling_rights,
-              std::string &en_passant,
-              std::string &halfmove_clock,
-              std::string &fullmove_number,
-              Board &board);
-void goCommand(std::vector<std::string> args, Board &board);
-
-// Parsers and Helpers
-std::optional<Move> parseMove(std::string move_uci, Board &board);
-
-void Uci::init()
+/**
+ * @brief Command Abstract class
+ * All commands should implement this class
+ *
+ */
+class Command
 {
-    Magics::init();
-    Tables::init();
-
-    Board board;
-
-    std::string cmd;
-    while (std::getline(std::cin, cmd))
-    {
-        parseCommand(cmd, board);
-    }
-}
+public:
+    virtual void execute(std::vector<std::string> &args, Board &board) = 0;
+};
 
 /**
- * @brief Parses and executes a uci compliant command
+ * @brief Handles display command
  *
- * @param args
- * @param board
  */
-void parseCommand(std::string &cmd, Board &board)
+class DisplayCommand : public Command
 {
-    std::vector<std::string> args = Utils::tokenizeString(cmd);
-
-    if (args.size() == 0)
-    {
-        return;
-    }
-
-    if (args[0] == "position")
-    {
-        args.erase(args.begin());
-
-        positionCommand(args, board);
-    }
-    else if (args[0] == "d")
+public:
+    void execute(std::vector<std::string> &args, Board &board)
     {
         board.display();
     }
-    else if (args[0] == "go")
-    {
-        args.erase(args.begin());
-        goCommand(args, board);
-    }
-}
+};
 
 /**
- * @brief Parses and executes position uci command
+ * @brief Handles 'the' UCI uci command
  *
- * @param args
- * @param board
  */
-void positionCommand(std::vector<std::string> &args, Board &board)
+class UCICommand : public Command
 {
-    if (args.size() == 0)
+public:
+    void execute(std::vector<std::string> &args, Board &board)
     {
-        return;
+        std::cout << "id name Chess Engine\n";
+        std::cout << "id name Chess Engine\n";
+        std::cout << "uciok" << std::endl;
     }
+};
 
-    if (args[0] == "startpos")
+/**
+ * @brief Handles the UCI 'isready' command
+ *
+ */
+class IsReadyCommand : public Command
+{
+public:
+    void execute(std::vector<std::string> &args, Board &board)
     {
-        args.erase(args.begin());
+        std::cout << "readyok" << std::endl;
+    }
+};
 
+/**
+ * @brief Handles the UCI 'ucinewgame' command
+ *
+ */
+class UCINewGameCommand : public Command
+{
+public:
+    void execute(std::vector<std::string> &args, Board &board)
+    {
         board.setStartingPosition();
     }
-    else if (args[0] == "fen")
-    {
-        args.erase(args.begin());
+};
 
-        if (args.size() < 6)
+/**
+ * @brief Handles the UCI 'position' command
+ *
+ */
+class PositionCommand : public Command
+{
+public:
+    void execute(std::vector<std::string> &args, Board &board)
+    {
+        if (args.size() == 0)
         {
             return;
         }
 
-        try
+        if (args[0] == "startpos")
         {
-            parseFen(args[0], args[1], args[2], args[3], args[4], args[5], board);
-            args.erase(args.begin(), args.begin() + 6);
+            args.erase(args.begin());
+
+            board.setStartingPosition();
         }
-        catch (const std::exception &e)
+        else if (args[0] == "fen")
         {
-            return;
-        }
-    }
-    else
-    {
-        return;
-    }
+            args.erase(args.begin());
 
-    if (args.size() == 0)
-    {
-        return;
-    }
+            if (args.size() < 6)
+            {
+                return;
+            }
 
-    if (args[0] == "moves")
-    {
-        args.erase(args.begin());
-
-        movesSubCommand(args, board);
-    }
-}
-
-/**
- * @brief Parses and executes moves uci subcommand
- *
- * @param moves
- * @param board
- */
-void movesSubCommand(std::vector<std::string> moves, Board &board)
-{
-    for (std::string move_uci : moves)
-    {
-        std::optional<Move> parsed_move = parseMove(move_uci, board);
-        if (parsed_move.has_value())
-        {
-            board.makeMove(parsed_move.value());
-        }
-    }
-}
-
-/**
- * @brief Parses fen string and sets board accordingly
- *
- * @param piece_placements
- * @param active_color
- * @param castling_rights
- * @param en_passant
- * @param halfmove_clock
- * @param fullmove_number
- * @param board
- */
-void parseFen(std::string &piece_placements, std::string &active_color, std::string &castling_rights, std::string &en_passant, std::string &halfmove_clock, std::string &fullmove_number, Board &board)
-{
-    static const std::regex piece_placements_regex(R"((([pnbrqkPNBRQK1-8]{1,8})\/?){8})");
-    static const std::regex active_color_regex(R"(b|w)");
-    static const std::regex castling_rights_regex(R"(-|K?Q?k?q?)");
-    static const std::regex en_passant_regex(R"(-|[a-h][3-6])");
-    static const std::regex halfmove_clock_regex(R"(\d+)");
-    static const std::regex fullmove_number_regex(R"(\d+)");
-    if (!std::regex_match(piece_placements, piece_placements_regex) ||
-        !std::regex_match(active_color, active_color_regex) ||
-        !std::regex_match(castling_rights, castling_rights_regex) ||
-        !std::regex_match(en_passant, en_passant_regex) ||
-        !std::regex_match(halfmove_clock, halfmove_clock_regex) ||
-        !std::regex_match(fullmove_number, fullmove_number_regex))
-    {
-        throw std::invalid_argument("Invalid fen string.");
-    }
-
-    board.setFromFen(piece_placements, active_color, castling_rights, en_passant, halfmove_clock, fullmove_number);
-}
-
-/**
- * @brief Executes the uci go command
- *
- * @param args
- * @param board
- */
-void goCommand(std::vector<std::string> args, Board &board)
-{
-    if (args.size() == 0)
-    {
-        return;
-    }
-
-    if (args[0] == "depth")
-    {
-        args.erase(args.begin());
-        int depth = 1;
-
-        if (args.size() != 0)
-        {
             try
             {
-                depth = std::stoi(args[0]);
+                handleFenPosition(args[0], args[1], args[2], args[3], args[4], args[5], board);
+                args.erase(args.begin(), args.begin() + 6);
             }
             catch (const std::exception &e)
             {
                 return;
             }
-
-            if (depth < 0)
-            {
-                return;
-            }
-
-            std::cout << depth << "\n";
         }
-        args.erase(args.begin());
+        else
+        {
+            return;
+        }
+
+        if (args.size() == 0)
+        {
+            return;
+        }
+
+        if (args[0] == "moves")
+        {
+            args.erase(args.begin());
+
+            handleMoves(args, board);
+        }
     }
-    else
+
+private:
+    void handleMoves(std::vector<std::string> &moves, Board &board)
     {
-        int depth = 6;
+        for (std::string move_uci : moves)
+        {
+            std::optional<Move> parsed_move = parseMove(move_uci, board);
+            if (parsed_move.has_value())
+            {
+                board.makeMove(parsed_move.value());
+            }
+        }
     }
-}
+
+    void handleFenPosition(std::string &piece_placements, std::string &active_color, std::string &castling_rights, std::string &en_passant, std::string &halfmove_clock, std::string &fullmove_number, Board &board)
+    {
+        static const std::regex piece_placements_regex(R"((([pnbrqkPNBRQK1-8]{1,8})\/?){8})");
+        static const std::regex active_color_regex(R"(b|w)");
+        static const std::regex castling_rights_regex(R"(-|K?Q?k?q?)");
+        static const std::regex en_passant_regex(R"(-|[a-h][3-6])");
+        static const std::regex halfmove_clock_regex(R"(\d+)");
+        static const std::regex fullmove_number_regex(R"(\d+)");
+        if (!std::regex_match(piece_placements, piece_placements_regex) ||
+            !std::regex_match(active_color, active_color_regex) ||
+            !std::regex_match(castling_rights, castling_rights_regex) ||
+            !std::regex_match(en_passant, en_passant_regex) ||
+            !std::regex_match(halfmove_clock, halfmove_clock_regex) ||
+            !std::regex_match(fullmove_number, fullmove_number_regex))
+        {
+            throw std::invalid_argument("Invalid fen string.");
+        }
+
+        board.setFromFen(piece_placements, active_color, castling_rights, en_passant, halfmove_clock, fullmove_number);
+    }
+
+    std::optional<Move> parseMove(std::string move_uci, Board &board)
+    {
+        for (Move const &move : Movegen::generateLegalMoves(board))
+        {
+            if (move.getUCI() == move_uci)
+            {
+                return move;
+            }
+        }
+
+        return std::nullopt;
+    }
+};
 
 /**
- * @brief Parses move in uci notation
+ * @brief Handles the UCI 'go' command
  *
- * @param move_uci
- * @param board
- * @return std::optional<Move>
  */
-std::optional<Move> parseMove(std::string move_uci, Board &board)
+class GoCommand : public Command
 {
-    for (Move const &move : Movegen::generateLegalMoves(board))
+public:
+    void execute(std::vector<std::string> &args, Board &board)
     {
-        if (move.getUCI() == move_uci)
+        int depth = 6;
+
+        if (args.size() != 0 && args[0] == "depth")
         {
-            return move;
+            args.erase(args.begin());
+            int depth = 1;
+
+            if (args.size() != 0)
+            {
+                try
+                {
+                    depth = std::stoi(args[0]);
+                }
+                catch (const std::exception &e)
+                {
+                    return;
+                }
+
+                if (depth < 0)
+                {
+                    return;
+                }
+
+                std::cout << depth << "\n";
+            }
+            args.erase(args.begin());
+        }
+
+        for (Move move : Movegen::generateLegalMoves(board))
+        {
+            std::cout << "bestmove " << move.getUCI() << std::endl;
+            break;
+        }
+    }
+};
+
+/**
+ * @brief Handles the UCI 'exit' command
+ *
+ */
+class QuitCommand : public Command
+{
+public:
+    void execute(std::vector<std::string> &args, Board &board)
+    {
+        exit(EXIT_SUCCESS);
+    }
+};
+
+/**
+ * @brief Processes incoming commands
+ *
+ */
+class CommandProcessor
+{
+private:
+    std::map<std::string, std::unique_ptr<Command>> _mapper;
+    Board _board;
+
+public:
+    static CommandProcessor &Instance()
+    {
+        static CommandProcessor cp;
+        return cp;
+    }
+
+    void init()
+    {
+        std::string cmd;
+        while (std::getline(std::cin, cmd))
+        {
+            std::vector<std::string> args = Utils::tokenizeString(cmd);
+            if (args.size() == 0)
+            {
+                continue;
+            }
+
+            auto command = _mapper.find(args[0]);
+            if (command != _mapper.end())
+            {
+                args.erase(args.begin());
+                command->second->execute(args, _board);
+            }
         }
     }
 
-    return std::nullopt;
+private:
+    CommandProcessor()
+    {
+        _mapper["uci"] = std::unique_ptr<Command>(new UCICommand);
+        _mapper["isready"] = std::unique_ptr<Command>(new IsReadyCommand());
+        _mapper["ucinewgame"] = std::unique_ptr<Command>(new UCINewGameCommand());
+        _mapper["position"] = std::unique_ptr<Command>(new PositionCommand());
+        _mapper["d"] = std::unique_ptr<Command>(new DisplayCommand());
+        _mapper["go"] = std::unique_ptr<Command>(new GoCommand());
+        _mapper["quit"] = std::unique_ptr<Command>(new QuitCommand());
+
+        _board.setStartingPosition();
+    }
+};
+
+void Uci::init()
+{
+    Magics::init();
+    Tables::init();
+    CommandProcessor::Instance().init();
 }
