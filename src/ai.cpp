584b1a100e317fd7cde901de175afe78406dc922
João Silveira <joao.freixial.silveira@tecnico.ulisboa.pt>
2022-04-27 16:44:49 +0100

    started ai #11

---
+++ src/ai.cpp
@@ -0,0 +1,17 @@
+#include "ai.hpp"
+
+#include "move.hpp"
+#include "board.hpp"
+
+Move AI::find_best_move()
+{
+    // calls search(alpha, beta, level)
+    return Move(0, 0, 0, 0, 0, 0, 0, 0);
+}
+
+int AI::search(int alpha, int beta, int level)
+{
+    // implements negamax
+    // call eval(this->board);
+    return 1;
+}
---
+++ src/ai.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+class Board;
+class Move;
+
+class AI
+{
+public:
+    AI(Board &board) : board(board) {}
+
+    Move find_best_move();
+
+private:
+    Board &board;
+
+    int search(int alpha, int beta, int level);
+};
