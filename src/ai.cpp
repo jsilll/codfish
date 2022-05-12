485a2b9e99d1784eb9905c9547501075c4ac4758
henrique <hdh.caraca@gmail.com>
2022-05-12 18:17:28 +0100

    implemented negamax

--- src/ai.cpp
+++ src/ai.cpp
@@ -14,8 +14,6 @@ Move AI::find_best_move()
     int best_val = std::numeric_limits<int>::min();
     Move best_move = Move(0, 0, 0, 0, 0, 0, 0, 0);
 
-    std::cout << "best_val" << best_val << std::endl;
-
     for (Move move : Movegen::generatePseudoLegalMoves(_board))
     {
         Board backup = _board;
@@ -24,13 +22,10 @@ Move AI::find_best_move()
         int attacker_side = backup.getSideToMove();
         if (!backup.isSquareAttacked(king_sq, attacker_side))
         {
-            int score = -search(std::numeric_limits<int>::min(), std::numeric_limits<int>::max(), 5, backup);
-
-            std::cout << "score for " << move.getUCI() << score << std::endl;
+            int score = -search(std::numeric_limits<int>::min(), std::numeric_limits<int>::max(), 4, backup);
 
             if (score > best_val)
             {
-                std::cout << "entrei aqui ;)" << std::endl;
                 best_val = score;
                 best_move = move;
             }
@@ -50,15 +45,6 @@ int AI::search(int alpha, int beta, int depth, Board &board)
     int best_val = std::numeric_limits<int>::min();
     for (const Move &move : Movegen::generatePseudoLegalMoves(board))
     {
-        if (best_val >= beta)
-        {
-            break;
-        }
-        if (best_val > alpha)
-        {
-            alpha = best_val;
-        }
-
         Board backup = board;
         backup.makeMove(move);
         int king_sq = Utils::bitScanForward(backup.getPieces(backup.getOpponent(), KING));
@@ -70,6 +56,18 @@ int AI::search(int alpha, int beta, int depth, Board &board)
             {
                 best_val = score;
             }
+            if (score >= beta)
+            {
+                break;
+            }
+            if (score > alpha)
+            {
+                alpha = score;
+            }
+            // if (alpha > beta)
+            // {
+            //     break;
+            // }
         }
     }
 
